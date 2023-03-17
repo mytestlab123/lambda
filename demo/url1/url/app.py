aa9dd9240e939f5cbb2b8da505422497ddc74718
Amit Karpe <amitkarpe@gmail.com>
2023-03-17 19:32:30 +0000

    Working Lambda Function URL successful ✅. But still need some improvements.

--- demo/url1/url/app.py
+++ demo/url1/url/app.py
@@ -1,17 +1,44 @@
 import json
 
 def lambda_handler(event, context):
-    event = json.dumps(event['body'])
-    event = json.loads(event)
-    print (event)
+    org = event
+    print (list(event.keys())[0])
+    print (list(event.keys())[1])
+    print (list(event.keys())[2])
+
+    print (event.keys())
+    print (event.values())
     print ("===============")
-    # print (json.loads(event))
-    key1 = event['key1']
-    print (key1)
+    # print (event['body'])
+    # exit (0)
+
+    # print (json.dumps(event))
+    # print (json.dumps(event['body']))
+    # print ("===============")
+    # print (json.dumps(event['body'])['key1'])
+    # print ("===============")
+    # print (json.loads(event['body'])['key2'])
+    # print ("===============")
+    # # key1 = json.loads(event['body'])['key1']
+    # print (json.loads(event['body']))
+    # # print ((event['body'])['key1'])
+    # print ("===============")
+    # print (json.loads(event['body'])['key1'])
+    # key1 = json.loads(event['body']['key1'])
+    # print (key1)
+    # key2 = json.dumps(event['body'])['key2']
+    # print (key2)
+    # event = json.dumps(event['body'])
+    # event = json.loads(event)
+    # print (event)
+    # print ("===============")
+    # # print (json.loads(event))
+    # key1 = event['key1']
+    # print (key1)
     return {
         "isBase64Encoded": "false",
         "statusCode": 200,
-        "body": event,
+        "body": json.dumps(org),
         "headers": {
             "content-type": "application/json"
         }
