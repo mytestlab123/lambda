98607036d1bebf0a97c73e17d404edca81ecf14a
Amit Karpe <amitkarpe@gmail.com>
2023-03-23 08:01:39 +0000

    Created lambda template by copying existing lambda code.

---
+++ main/opti/TODO.md
@@ -0,0 +1,8 @@
+# Tasks List
+
+# TODO: Created lambda template by copying existing lambda code
+# TODO: Created new Portfolio, upload on github
+# TODO: Create "secret.json" with appropriate values
+# TODO: Update code for new chain 
+# TODO: Add fund into given wallet and test with minimum investment amount for small portfolio.
+
---
+++ main/opti/main/list.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# List all tokens on 1inch for given blockchain
+# Usage: ./list.sh
+# URL: https://docs.1inch.io/docs/aggregation-protocol/api/swagger/
+# Code for [chainid](https://github.com/RichardAtCT/1inch_wrapper/blob/daa2473a434a6dda4f6a2b0c8bfd4315b44024e9/oneinch_py/main.py#L21)
+# List of more [chains](https://chainlist.org/)
+#     chains = {
+    #     "ethereum": '1',
+    #     "binance": '56',
+    #     "polygon": "137",
+    #     "optimism": "10",
+    #     "arbitrum": "42161",
+    #     "gnosis": "100",
+    #     "avalanche": "43114",
+    #     "fantom": "250"
+    # }
+
+export chain_id=10
+curl -s https://api.1inch.io/v5.0/10/tokens | jq '.tokens[]' | grep "symbol" | cut -d '"' -f 4 | sort
